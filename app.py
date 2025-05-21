530c3d9f0223160cec3713996c03c9e3049d3c54
Issa Sukatu Abdullahi <sukaissa@gmail.com>
2025-05-21 09:54:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from utils.video_utils import extract_audio_from_url
 from utils.audio_utils import transcribe_audio
 from utils.accent_model import classify_accent
 
-st.title("English Accent Classification Tool")
+st.title("English Accent Classification Tool BY Issa Sukatu Abdullahi(sukaissa@gmail.com)")
 
 video_url = st.text_input("Enter Video URL")
 uploaded_file = st.file_uploader("Or browse for a video/audio file", type=["mp4", "mp3", "wav", "m4a"])
